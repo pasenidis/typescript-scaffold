e9ddd3074f9d3533bd6fdc48c373e0e0dcf6e69b
Edward Pasenidis <me@edpasenidis.tech>
2020-06-05 00:02:36 +0300

    style: fix fix fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-      ecmaVersion: 2020, 
-      sourceType: "module",
-      /* ecmaFeatures: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        /* ecmaFeatures: {
         jsx: true // Allows for the parsing of JSX
       } */
     },
@@ -15,14 +15,13 @@ module.exports = {
     }, */
 
     extends: [
-      // "plugin:react/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier/@typescript-eslint",
-      "plugin:prettier/recommended"
+        // "plugin:react/recommended",
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
     ],
-  
+
     rules: {
-      // "@typescript-eslint/explicit-function-return-type": "off",
+        // "@typescript-eslint/explicit-function-return-type": "off",
     },
-  
-  };
+};
